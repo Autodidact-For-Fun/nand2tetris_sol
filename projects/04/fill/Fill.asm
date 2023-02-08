f57d34a59930690abf262bfad630b40b1bd600de
amulil <amulil@126.com>
2023-02-08 23:03:14 +0800

    finish assemble program
    - Fill.asm
    - Mult.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,59 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LISTEN)
+  @KBD
+  D=M
+  @FILL
+  D;JNE
+  @UNFILL
+  D;JEQ
+
+(FILL)
+  @SCREEN
+  D=A
+  @start
+  M=D
+  @KBD
+  D=A
+  @end
+  M=D-1
+(LOOP1)
+  @start
+  D=M
+  @end
+  D=D-M
+  @LISTEN
+  D;JGT
+  @start
+  A=M
+  M=-1
+  @start
+  M=M+1
+  @LOOP1
+  0;JMP
+   
+(UNFILL)
+  @SCREEN
+  D=A
+  @start
+  M=D
+  @KBD
+  D=A
+  @end
+  M=D-1
+(LOOP2)
+  @start
+  D=M
+  @end
+  D=D-M
+  @LISTEN
+  D;JGT
+  @start
+  A=M
+  M=0
+  @start
+  M=M+1
+  @LOOP2
+  0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,34 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+@sum
+M=0
+@i
+M=0
+
+(LOOP)
+  @R1
+  D=M
+  @i
+  D=D-M
+  @SUM
+  D;JLE
+  @R0
+  D=M
+  @sum
+  M=M+D
+  @i
+  M=M+1
+  @LOOP
+  0;JMP
+
+(SUM)
+  @sum
+  D=M
+  @R2
+  M=D
+
+(END)
+  @END
+  0;JMP
